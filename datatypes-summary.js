d70d112659b638b4206922c4fe16934837aafbac
King Mudassir <144685827+kingmudassir@users.noreply.github.com>
2025-07-10 12:37:38 +0000

    coded

---
+++ datatypes-summary.js
@@ -0,0 +1,19 @@
+// Primitive Datatypes
+// 7 types: [String, Number, Boolean, Number, Null, Undefined, Symbol, BigInt]
+
+//Reference Type (Non-Primitive)
+// 3 types: [Arrays, Functions, Objects]
+const arraynames = ["muhammad", "mudassir", "lexus"];
+
+let obj = {
+    name:"muhammad",
+    age:22
+}
+
+const myFunction = function(){
+    console.log("Function successfully made!");
+}
+
+console.log(typeof obj);
+console.log(typeof myFunction);
+console.log(typeof arraynames);
