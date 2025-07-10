c115303e6ef416f4f7eef7e25448ad3636c7e920
King Mudassir <144685827+kingmudassir@users.noreply.github.com>
2025-07-10 10:50:55 +0000

    Variebles Cleared

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountID = 1234567890
+let accountEmail = "mudassir@gmail.com"
+var accountPassword = "1234567890"
+accountCity = "Islamabad"
+let accountState;
+let accountCondition = "";
+
+
+
+// console.log(accountID); console.log("\n");
+// console.log(accountEmail); console.log("\n");
+// console.log(accountPassword); console.log("\n");
+// console.log(accountCity); console.log("\n\n");
+
+//Changes Start Here
+//accountID = 1234
+accountEmail = "mud@gmail.com"
+accountPassword = "123"
+accountCity = "Pindi"
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState, accountCondition])
+
+// console.log(accountID); console.log("\n");
+// console.log(accountEmail); console.log("\n");
+// console.log(accountPassword); console.log("\n");
+// console.log(accountCity); console.log("\n");
