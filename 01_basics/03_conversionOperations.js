9ba631776b08fe27fee38154c93797d51a73c284
King Mudassir <144685827+kingmudassir@users.noreply.github.com>
2025-07-10 11:48:41 +0000

    coded

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,12 @@
+// let score = "Moody"
+// console.log(typeof(score)); console.log("\n");
+// console.log(typeof score); console.log("\n");
+
+// let valueInNumber = Number(score);
+// console.log(valueInNumber); console.log("\n");
+
+let regularValue
+let booleanConvert = Boolean(regularValue)
+console.log(booleanConvert); console.log("\n");
+console.log(typeof(booleanConvert)); console.log("\n");
+
